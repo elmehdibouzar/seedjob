46e3be1f18314d35bb6d9091cc900edb59489b95
elmehdibouzar <mehdi.bouzar@gmail.com>
2020-08-31 15:52:26 +0200

    Create ocpCreateTalendBuildPod.groovy

---
+++ ocpCreateTalendBuildPod.groovy
@@ -0,0 +1,15 @@
+def call(boolean dev = false, def body) {
+    // working directory in the image (aka home directory)
+    String workingDir = "/tmp"
+
+    baseTalendTemplate(workingDir,  dev) {
+        stage("SOFA configuration") {
+            container('build') {
+                sh "git config --global user.name SOFA"
+                sh "git config --global user.email service.bsc-sofa@socgen.com"
+            }
+        }
+
+        body()
+    }
+}
