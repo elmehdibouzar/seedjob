35844463ee929f0da33ab35f383e4524ddbd17f6
elmehdibouzar <mehdi.bouzar@gmail.com>
2020-09-01 22:52:21 +0200

    Update baseTalendTemplate.groovy

--- baseTalendTemplate.groovy
+++ baseTalendTemplate.groovy
@@ -1,19 +1,17 @@
 def call(String workingDir = "/tmp", boolean dev = false, def body) {
 
     def talendContainerTemplate = containerTemplate(name: 'build',
-            image: "*:${dev ? 'dev' : 'prod'}",
+            image: "image-registry.openshift-image-registry.svc:5000/test/openjdk-8",
             command: 'cat',
             ttyEnabled: true,
             workingDir: workingDir,
             alwaysPullImage: true,
             envVars: [
-                    secretEnvVar(key: 'NEXUS_V2_USERNAME', secretName: 'maven-secret', secretKey: 'NEXUS_V2_USERNAME'),
-                    secretEnvVar(key: 'NEXUS_V2_PASSWORD', secretName: 'maven-secret', secretKey: 'NEXUS_V2_PASSWORD'),
-
+                    secretEnvVar(key: 'NEXUS_V2_USERNAME', secretName: 'maven-secret', secretKey: 'NEXUS_V2_USERNAME')
             ])
 
-    def mavenVolume = persistentVolumeClaim(claimName: 'maven-repository-2', mountPath: '')
-    def talendlib = persistentVolumeClaim(claimName: 'talend-lib', mountPath: '/sofa/talend-lib')
+#    def mavenVolume = persistentVolumeClaim(claimName: 'maven-repository-2', mountPath: '')
+#    def talendlib = persistentVolumeClaim(claimName: 'talend-lib', mountPath: '/sofa/talend-lib')
 
     baseTemplate(true, workingDir, [talendContainerTemplate], [talendlib,mavenVolume]) {
         body()
