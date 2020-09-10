c43b50e989a9cce568c8b8dd2791802d09b850af
elmehdibouzar <mehdi.bouzar@gmail.com>
2020-09-10 15:57:40 +0200

    Update baseTalendTemplate.groovy

--- vars/baseTalendTemplate.groovy
+++ vars/baseTalendTemplate.groovy
@@ -6,9 +6,7 @@ def call(String workingDir = "/tmp", boolean dev = false, def body) {
             ttyEnabled: true,
             workingDir: workingDir,
             alwaysPullImage: true,
-            envVars: [
-                    secretEnvVar(key: 'NEXUS_V2_USERNAME', secretName: 'maven-secret', secretKey: 'NEXUS_V2_USERNAME')
-            ])
+)
 
 //   def mavenVolume = persistentVolumeClaim(claimName: 'maven-repository-2', mountPath: '')
 //   def talendlib = persistentVolumeClaim(claimName: 'talend-lib', mountPath: '/sofa/talend-lib')
